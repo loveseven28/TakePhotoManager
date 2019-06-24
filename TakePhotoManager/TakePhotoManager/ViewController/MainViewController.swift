bf197939fd8829e2111144cc450887bd606f70ea
hung dang <danghung.nt@gmail.com>
2019-06-24 15:21:35 +0700

    commit

--- TakePhotoManager/TakePhotoManager/ViewController/MainViewController.swift
+++ TakePhotoManager/TakePhotoManager/ViewController/MainViewController.swift
@@ -7,14 +7,28 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MainViewController: BaseViewController {
-
+    var locationManager: CLLocationManager?
+    @IBOutlet weak var tamOcButton: UIButton!
+    @IBOutlet weak var tamOcConstraintTop: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         initMenuView()
         setGradientNavi()
+        locationManager = CLLocationManager()
+        locationManager?.delegate = self
+        locationManager?.requestWhenInUseAuthorization()
+        locationManager?.showsBackgroundLocationIndicator = false
+        
+        tamOcConstraintTop.constant = 50
+        UIView.animate(withDuration: 0.5) {
+            self.view.layoutIfNeeded()
+        }
     }
     
     @IBAction func menuButtonTapped(_ sender: UIButton) {
@@ -23,5 +37,22 @@ class MainViewController: BaseViewController {
 
     @IBAction func hashTagButtonTapped(_ sender: Any) {
         
+    }
+}
+
+extension MainViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            // you're good to go!
+            locationManager?.startUpdatingLocation()
+            locationManager!.allowsBackgroundLocationUpdates = true
+            locationManager!.pausesLocationUpdatesAutomatically = false
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            print("New location is \(location)")
+        }
     }
 }
